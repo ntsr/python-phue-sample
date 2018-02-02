6f2fde45afca363301561dce9b4348e820526e56
naoki_tsuru <naoki_tsuru@balmuda.com>
2018-02-02 11:59:36 +0900

    Add show_hue and set_hue

--- README.md
+++ README.md
@@ -1,21 +1,52 @@
 # phue sample
 
+Show and update Philips hue.
+
 ## env
 Python 3.6.3
 
 ## setup
+
 1. Install python packages via pip.
-```
+
+```sh
 $ virtualenv venv
 $ . venv/bin/activate
 (venv)$ pip install -r requirements.txt
 ```
 
-2. Config python_hue.json
-Make `python_hue.json` from `python_hue.json.sample`.
-Enter HomeBridge IP address.
+2. Connect your HomeBridge and phone in the same LAN.
+3. Setup Hue App on your phone and find IP of HomeBridge.
 
 ## Run
+
+### demo
+
+```sh
+(venv) $ python hue.py demo --ip [HomeBridge_IP] -i [light_id]
 ```
-(venv) $ python hue.py
+
+### show current value
+
+```sh
+(venv) $ python hue.py show --ip [HomeBridge_IP] -i [light_id]
+```
+
+### update value
+
+```sh
+(venv) $ python hue.py set --ip [HomeBridge_IP] -i [light_id] --bri [0-255] --color-rgb-hex [rrggbb]
+```
+
+### Turn off
+
+```sh
+(venv) $ python hue.py set --ip [HomeBridge_IP] -i [light_id] --off
+```
+
+
+### help
+
+```sh
+(venv) $ python hue.py --help
 ```
--- hue.py
+++ hue.py
@@ -4,56 +4,205 @@
 https://github.com/studioimaginaire/phue
 """
 
-from phue import Bridge
-from rgbxy import Converter
 import time
-CONFIG_PATH = 'python_hue.json'
+import argparse
+from phue import Bridge, PhueRegistrationException, Light
+from rgbxy import Converter
+
+DEFAULT_CONFIG_FILE_PATH = './python_hue.json'
+GET_INTERVAL = 0.5
+SET_INTERVAL = 1
 
 conv = Converter()
 
-b = Bridge(config_file_path=CONFIG_PATH)
 
-# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
-b.connect()
+def get_args():
+    ap = argparse.ArgumentParser(description='Philips Hue Developer Tool.')
+    ap.add_argument('action', \
+        action='store', \
+        nargs=None, \
+        const=None, \
+        default=None, \
+        type=str, \
+        choices=None, \
+        help='Action', \
+        metavar=None)
+    ap.add_argument('--ip', \
+        action='store', \
+        nargs='?', \
+        const=None, \
+        default=None, \
+        type=str, \
+        choices=None, \
+        help='HomeBridge IP address', \
+        metavar=None)
+    ap.add_argument('-i', '--light-id', \
+        action='store', \
+        nargs='?', \
+        const=None, \
+        default=None, \
+        type=int, \
+        choices=None, \
+        help='Light device No.', \
+        metavar=None)
+    ap.add_argument('--off', \
+        action='store_true', \
+        help='Light off')
+    ap.add_argument('-b', '--bri', \
+        action='store', \
+        nargs='?', \
+        const=None, \
+        default=None, \
+        type=int, \
+        choices=None, \
+        help='Brightness', \
+        metavar=None)
+    ap.add_argument('-c', '--color-rgb-hex', \
+        action='store', \
+        nargs='?', \
+        const=None, \
+        default=None, \
+        type=str, \
+        choices=None, \
+        help='Color RGB Hex: RRGGBB', \
+        metavar=None)
+    ap.add_argument('--config-file-path',\
+        action='store', \
+        nargs='?', \
+        const=None, \
+        default=DEFAULT_CONFIG_FILE_PATH, \
+        type=str, \
+        choices=None, \
+        help='Config file path', \
+        metavar=None)
 
-light = b.get_light_objects()[0]
+    args = ap.parse_args()
 
-def show_xy():
+    # TODO validation
+
+    return args
+
+
+def get_bridge(ip, config_file_path) -> Bridge:
+    if ip is None:
+        print('no IP set!')
+        exit()
+
     while True:
         try:
-            print(light.xy)
-            time.sleep(0.3) 
-
+            b = Bridge(ip=ip, config_file_path=config_file_path)
+            b.connect()
+            return b
+        except PhueRegistrationException:
+            print('Searching... Press HomeBridge Button.')
+            time.sleep(1)
+            continue
         except KeyboardInterrupt:
-            break
+            print('KeyboardInterrupt')
+            exit()
+        except Exception as e:
+            print(e)
+            exit()
 
-    print('end')
 
-def show_rgb():
-    while True:
-        try:
-            xy = light.xy
-            bri = light.brightness
-            color_rgb = conv.xy_to_rgb(*xy, bri=bri)
-            color_hex = conv.xy_to_hex(*xy, bri=bri)
-            print("rgb:{}\thex:{}\txy:{}".format(color_rgb, color_hex, xy))
-            time.sleep(0.3) 
+def get_light(bridge, light_id) -> Light:
 
-        except KeyboardInterrupt:
-            break
+    lights = bridge.get_light_objects()
+    index = light_id - 1   # light_id is 1 origin
+    try:
+        return lights[index]
+    except:
+        print("invalid light_id! :{}".format(light_id))
+        return
 
-    print('end')
-    
 
 def main():
+
+    args = get_args()
+    bridge = get_bridge(ip=args.ip, config_file_path=args.config_file_path)
+    num = args.light_id
+    light = get_light(bridge, num) or exit()
+
+    if args.action == "show":
+        show_hue(light)
+    elif args.action == "set":
+        on = not args.off
+        bri = args.bri
+        rgb_hex = args.color_rgb_hex
+        set_hue(light, on, bri, rgb_hex)
+    else:
+        demo(light)
+
+
+def show_hue(light):
+    """
+    name, on, bri, rgb, hex, xy
+    """
+    while True:
+        try:
+            reachable = light.reachable
+            light_id = light.light_id
+            on = '--' if not reachable else 'on' if light.on else 'off'
+            bri = light.brightness if reachable else "--"
+            xy = light.xy if reachable else "--"
+            if xy != '--':
+                color_rgb = conv.xy_to_rgb(*xy, bri=bri)
+                color_hex = conv.xy_to_hex(*xy, bri=bri)
+
+            print("id:{}\t{}\tbri:{}\thex:{}\txy:{}".format(
+                light_id, on, bri, color_hex, xy))
+            time.sleep(GET_INTERVAL) 
+
+        except KeyboardInterrupt:
+            break
+
+    print('end')
+
+
+def set_hue(light, on=None, bri=None, rgb_hex=None):
+    if isinstance(on, bool):
+        light.on = on
+
+    if isinstance(bri, int) and 0 <= bri <= 255:
+        light.brightness = bri
+
+    if isinstance(rgb_hex, str):
+        try:
+            x, y = conv.hex_to_xy(rgb_hex)
+            light.xy = (x, y)
+        except:
+            print('Invalid rgb_hex!: {}'.format(rgb_hex))
+
+
+def demo(light):
     color_hex = '0000ff'
     xy = conv.hex_to_xy(color_hex)
-    print(xy)
+
+    light.on = True
     light.xy = xy
+    light.brightness = 50
+    time.sleep(SET_INTERVAL)
 
-    show_rgb()
+    import random
+    for _ in range(10):
+        x = random.random()
+        y = random.random()
+        xy = (x, y)
+        print(xy)
+        light.xy = xy
+        time.sleep(SET_INTERVAL)
 
-def test():
+    light.on = False
+    print("done")
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+Sample snippets
+
     # Get the bridge state (This returns the full dictionary that you can explore)
     b.get_api()
 
@@ -87,5 +236,4 @@ def test():
     command =  {'transitiontime' : 300, 'on' : True, 'bri' : 254}
     b.set_light(1, command)
 
-if __name__ == '__main__':
-    main()
+"""
